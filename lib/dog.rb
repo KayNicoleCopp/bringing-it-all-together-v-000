de9a1cea41f9f3a7d78d582b499760015b8ec1e9
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-03-01 15:26:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,9 @@ def self.create_table
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
+
 
 
 
