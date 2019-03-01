3779b61f57ef144488b63c0c26cd93ffec12d3c9
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-03-01 15:19:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,15 @@ def initialize(id: nil, name:, breed:)
   @id = id
 end
 
+def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+
 end
